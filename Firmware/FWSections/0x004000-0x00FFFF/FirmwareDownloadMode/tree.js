0fb93c7a28676e2b6717eafc3ba7eef048ec0913
Andre <pandre4219@gmail.com>
2024-09-29 22:04:51 +0200

    Firmware download mode Webpage
    Got the Firmware download mode webpage from the running system

---
+++ Firmware/FWSections/0x004000-0x00FFFF/FirmwareDownloadMode/tree.js
@@ -0,0 +1,241 @@
+
+var lang = {
+"Apply":{
+de:"Übernehmen",
+fr:"Appliquer",
+zh:"应用",
+es:"Aplicar",
+ja:"適用"},
+"Cancel":{
+de:"Abbrechen",
+fr:"Annuler",
+zh:"取消",
+es:"Cancelar",
+ja:"キャンセル"},
+"Reboot":{
+de:"Neu starten",
+fr:"Redémarrer le système",
+zh:"重启",
+es:"Reiniciar",
+ja:"再起動"},
+"Browse":{
+de:"Durchsuchen",
+fr:"Parcourir",
+zh:"浏览",
+es:"Examinar",
+ja:"参照"},
+"Restart the switch.":{
+de:"Starten Sie den Switch neu.",
+fr:"Redémarrez le switch.",
+zh:"重新启动交换机。",
+es:"Reinicie el switch.",
+ja:"スイッチを再起動してください。"},
+"The device is restarting. Wait until the process is complete.":{
+de:"Das Gerät wird neu gestartet. Warten Sie, bis der Vorgang abgeschlossen ist.",
+fr:"L'appareil est en cours de redémarrage. Patientez jusqu'à ce que la procédure soit terminée.",
+zh:"设备在重新启动。请等待此过程完成。",
+es:"Se está reiniciando el dispositivo. Espere hasta que se haya completado el proceso.",
+ja:"デバイスを再起動しています。処理が完了するまでお待ちください。"},
+"Refresh to reload.":{
+de:"Aktualisieren, um neu zu laden.",
+fr:"Actualiser pour recharger.",
+zh:"刷新以重新加载。",
+es:"Actualice para volver a cargar.",
+ja:"更新して再読み込みします。"},
+"Firmware Update":{
+de:"Firmware-Aktualisierung",
+fr:"Mise à jour du micrologiciel (firmware)",
+zh:"固件更新",
+es:"Actualización de firmware",
+ja:"ファームウェア アップデート"},
+"Select the file to update the firmware:":{
+de:"Wählen Sie die Datei aus, um die Firmware zu aktualisieren:",
+fr:"Sélectionnez le fichier pour mettre à jour le micrologiciel (firmware) :",
+zh:"选择文件以更新固件：",
+es:"Seleccione el archivo para actualizar el firmware:",
+ja:"ファイルを選択して、ファームウェアをアップデートします。"},
+"Firmware download complete. Wait for the device to restart in order for the new firmware to take effect.":{
+de:"Firmware-Download abgeschlossen. Warten Sie, bis das Gerät neu gestartet ist, damit die neue Firmware zum Tragen kommt.",
+fr:"Téléchargement du firmware terminé. Patientez jusqu'au redémarrage de l'appareil pour que le nouveau firmware soit effectif.",
+zh:"固件下载完成。请等待设备重新启动，以使新固件生效。",
+es:"Descarga de firmware completada. Espere a que el dispositivo se reinicie para que el nuevo firmware surta efecto.",
+ja:"ファームウェアのダウンロードが完了しました。新しいファームウェアを有効にするために、デバイスが再起動するまでお待ちください。"},
+"Warning: Do not interrupt the network connection or power to the switch, or refresh or close the browser window during this process. Do not disconnect any Ethernet cables or power off the switch until the process is complete.":{
+de:"Achtung: Unterbrechen Sie während dieses Vorgangs nicht die Netzwerkverbindung oder die Stromversorgung zum Switch und schließen bzw. aktualisieren Sie auch nicht das Browserfenster. Trennen Sie keine Ethernet-Kabel und schalten Sie den Switch nicht aus, bis der Vorgang abgeschlossen ist.",
+fr:"Avertissement : N'interrompez pas la connexion réseau, l'alimentation du switch ou actualisez ou fermez la fenêtre du navigateur lors de ce processus. Ne débranchez pas les câbles Ethernet et n'éteignez pas le switch avant la fin du processus.",
+zh:"警报: 在此过程中请勿中断交换机的网络连接或电源，或是刷新或关闭浏览器窗口。整个过程完成前，请勿断开任何以太网线缆或关闭交换机电源。",
+es:"Advertencia: No desconecte el switch de la red, no lo apague ni cierre o actualice la ventana del navegador durante este proceso. No desconecte ningún cable Ethernet o de alimentación del switch hasta que haya finalizado el proceso.",
+ja:"警告: この処理中にネットワーク接続を切断したり、スイッチの電源を切ったりしないでください。また、ブラウザーウィンドウを更新したり、閉じたりしないでください。処理が完了するまで、LANケーブルを抜いたり、スイッチの電源を切ったりしないでください。"},
+"An error occurred while updating the file. Select a valid file and try again.":{
+de:"Beim Aktualisieren der Datei ist ein Fehler aufgetreten. Wählen Sie eine gültige Datei aus, und versuchen Sie es erneut.",
+fr:"Une erreur s'est produite lors de la mise à jour du fichier. Sélectionnez un fichier valide, puis réessayez.",
+zh:"更新文件时发生错误。选择有效文件，然后重试。",
+es:"Se ha producido un error durante la actualización del archivo. Seleccione un archivo válido e inténtelo de nuevo.",
+ja:"ファイルのアップデート中にエラーが発生しました。有効なファイルを選択してもう一度お試しください。"},
+"Transferring firmware...":{
+de:"Firmware wird übertragen...",
+fr:"Transfert du firmware en cours...",
+zh:"正在传输固件...",
+es:"Transfiriendo el firmware...",
+ja:"ファームウェアを転送しています..."},
+"© NETGEAR, Inc. All rights reserved.":{
+de:"© NETGEAR, Inc. Alle Rechte vorbehalten.",
+fr:"© NETGEAR, Inc. Tous droits réservés.",
+zh:"© NETGEAR, Inc. 保留所有权利。",
+es:"© NETGEAR, Inc. Todos los derechos reservados.",
+ja:"© NETGEAR, Inc. All rights reserved."},
+"GS105PE – 5-Port Gigabit Ethernet Smart Managed Plus Switch (PoE Pass-Thru)":{
+de:"GS105PE – 5-Port Gigabit Ethernet Smart Managed Plus Switch (PoE PassThru)",
+fr:"GS105PE – Switch Gigabit ProSAFE Plus 5 ports avec intercommunication POE",
+zh:"GS105PE – 5 端口千兆 ProSAFE Plus 交换机，PoE 直通",
+es:"GS105PE: switch ProSAFE Plus de 5 puertos Gigabit con PoE Pass Through",
+ja:"GS105PE – ギガビット5ポート アンマネージプラス・スイッチ (PoEパススルー対応)"},
+"GS305E – 5-Port Gigabit Ethernet Smart Managed Plus Switch":{
+de:"GS305E – 5-Port Gigabit Ethernet Smart Managed Plus Switch",
+fr:"GS305E – Switch 5 Gigabit ProSAFE Plus 5 ports",
+zh:"GS305E – 5 端口千兆 ProSAFE Plus 交换机",
+es:"GS305E: switch ProSAFE Plus de 5 puertos Gigabit",
+ja:"GS305E – ギガビット5ポート アンマネージプラス・スイッチ"}
+};
+function transLang() {
+var eles = document.getElementsByClassName('lang'),
+len = eles.length,
+i, key, e;
+var langList = ['en','en','zh','fr','de','ja','es'],
+langType = top.document.getElementById('langType').value;
+langType = !!langList[langType] ? langList[langType] : 'en';
+if(langType === 'en') {
+document.documentElement.style.opacity = 1;
+return;
+}
+for (i = 0; i < len; i++){
+e = eles[i];
+if (e.tagName == "INPUT" && e.type == "button") {
+e.value = trans(e.value);
+} else {
+e.innerHTML = trans(e.innerHTML);
+}
+}
+document.documentElement.style.opacity = 1;
+}
+function trans(str) {
+var langList = ['en','en','zh','fr','de','ja','es'],
+langType = top.document.getElementById('langType').value;
+langType = !!langList[langType] ? langList[langType] : 'en';
+if (langType === 'en') {
+return str;
+}
+if (lang[str]) {
+return lang[str][langType];
+} else {
+return str;
+}
+}
+document.addEventListener("DOMContentLoaded", transLang, false);
+
+function checkIPv4Address(field)
+{
+var obj=field.value;
+var exp=/^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/;
+var reg = obj.match(exp);
+if(reg==null)
+{
+return false;
+}
+else
+{
+return true;
+}
+}
+function CreateButtons(type,butname,fntn,btnid,optn)
+{
+butname = trans(butname);
+if((optn == "off") ||(optn == ""))
+{
+str = "<input type='"+type+"' class='buttonsOff' name='"+btnid+"' id='"+btnid+"' value='"+butname+"' onclick='"+fntn+"'>";
+}
+else if(optn == "on")
+{
+str = "<input type='"+type+"' class='buttonsOn'  id='"+btnid+"' name='"+btnid+"' title='"+butname+"' value='"+butname+"' onclick='"+fntn+"'>";
+}
+return str;
+}
+function PaintButtons(str){
+var Prntid = top.document.getElementById("ButtonsDiv");
+if(Prntid!=null){Prntid.innerHTML=str;
+top.transLang();
+return;
+}
+}
+function submitForm()
+{
+var form1=window.frames['maincontent'].document.forms[0];
+var ip = window.frames['maincontent'].document.getElementById("Ip_Address");
+if(checkIPv4Address(ip) == 0)
+{
+alert("IP address: " + ip + " is not a valid IP address.")
+ip.value="";
+ip.focus();
+return;
+}
+form1.submit();
+}
+function submitHttpForm()
+{
+var form1=window.frames['maincontent'].document.forms[0];
+var filediv = window.frames['maincontent'].document.getElementById("fileDiv");
+disableImage();
+form1.submit();
+filediv.innerHTML="<p style='margin-bottom: 5px;'>"+trans('Transferring firmware...')+"</p><p>"+trans('Warning: Do not interrupt the network connection or power to the switch, or refresh or close the browser window during this process. Do not disconnect any Ethernet cables or power off the switch until the process is complete.')+"</p>";
+}
+function submitRebootForm()
+{
+var form1=window.frames['maincontent'].document.forms[0];
+form1.submit();
+}
+function cancelButton()
+{
+var form1=window.frames['maincontent'].document.forms[0];
+disableImage();
+form1.reset();
+}
+function mouserOverLink(ths){
+if((ths.parentNode.className == 'mouOu')||(ths.parentNode.className == '')){
+ths.parentNode.className = 'mouOv';
+}
+}
+function mouseOutLink(ths){
+if(ths.parentNode.className == 'mouOv'){
+ths.parentNode.className = 'mouOu';
+}
+}
+function tbhdr(tlt, helpUrl)
+{
+var str = "<div class='sec_header'><span class='tableTitleBottom font11purplebold leftAlign lang'>"+tlt;
+str += "</span></div>";
+document.write(str);
+}
+function enableImage()
+{
+var str = CreateButtons('button','Cancel','cancelButton()','btn_Cancel','on');
+str += CreateButtons('button','Apply','submitForm()','btn_Apply','on');
+PaintButtons(str);
+}
+function disableImage()
+{
+var str = CreateButtons('button','Cancel','javaScript:void(0)','btn_Cancel','off');
+str += CreateButtons('button','Apply','javaScript:void(0)','btn_Apply','off');
+PaintButtons(str);
+}
+function handleFourNav(){
+var tree_hrefs = [];
+tree_hrefs = document.getElementById('thirdNav').getElementsByTagName('a');
+for(var i=0;i < tree_hrefs.length;i++){
+tree_hrefs[i].onclick = function(){
+for(var j=0;j<tree_hrefs.length;j++){
+tree_hrefs[j].parentNode.className = '';
+}
+this.parentNode.className = 'clicked';
+}
+}
+}
